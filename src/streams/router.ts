208a070889bfeab031269d5675e538e06a344d96
Timur <timurkayzer@gmail.com>
2022-09-25 12:42:15 +0500

    promise lists

--- src/streams/router.ts
+++ src/streams/router.ts
@@ -5,6 +5,7 @@ import { Stream } from 'node:stream';
 
 const streamsRouter = Router();
 
+
 streamsRouter.get('/streams/test-file', async (req, res) => {
     let str = "";
     const readable = fs.createReadStream(path.resolve('./src/streams/testfile.txt'), { encoding: 'utf8' });
@@ -34,6 +35,36 @@ streamsRouter.get('/streams/for-await', async (req, res) => {
         for await (const chunk of promises) {
             console.log(chunk);
         }
+    }
+    catch (e) {
+        console.error(e);
+    }
+
+    res.end();
+});
+
+streamsRouter.get('/streams/promise-lists', async (req, res) => {
+
+    const successPromise = new Promise((resolve, reject) => {
+        resolve("Успешное выполнение");
+    });
+
+    const failedPromise = new Promise((resolve, reject) => {
+        reject("Ошибка");
+    });
+
+    try {
+        // const promises = await Promise.all([successPromise, failedPromise, successPromise]);
+        const promises = await Promise.allSettled([successPromise, failedPromise, successPromise]);
+
+
+        for (const chunk of promises) {
+            console.log(chunk);
+        }
+
+        const promise = await Promise.race([successPromise, failedPromise, successPromise]);
+        console.log(promise);
+
     }
     catch (e) {
         console.error(e);
