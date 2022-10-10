5fcfd0034e24af0b98876fa32edca6175f2b6194
Timur <timurkayzer@gmail.com>
2022-10-11 00:12:34 +0500

    intermittent socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,61 @@
 import * as express from "express";
-import { PORT } from "./constants";
+import http from 'http';
+import { Server } from "socket.io";
 import { eventEmitterRouter } from "./eventemitter/router";
 import { streamsRouter } from "./streams/router";
-import { Server } from 'ws';
+import { Server as WsServer } from 'ws';
+import { PORT } from "./constants";
 
-const wsServer = new Server({
-    port: 5000
-});
+try {
+    const app = express();
 
-wsServer.on('connection', (wsClient) => {
-    wsClient.send('Test');
-    console.log('Test');
-    wsClient.on('message', (message) => {
-        wsClient.send('Message accepted');
-        console.log(message?.toString());
+    const server = http.createServer(app);
+
+    const io = new Server(server);
+    io.on('connection', (socket) => {
+        console.log('Aaaaa');
     });
 
-    wsClient.on('close', () => {
-        console.log(`Connection closed`);
+    const wsServer = new WsServer({
+        port: 5000
     });
-});
+
+    wsServer.on('connection', (wsClient) => {
+        wsClient.send('Test');
+        console.log('Test');
+        wsClient.on('message', (message) => {
+            wsClient.send('Message accepted');
+            console.log(message?.toString());
+        });
+
+        wsClient.on('close', () => {
+            console.log(`Connection closed`);
+        });
+    });
 
 
-const app = express();
+    console.log('Server started on port ' + PORT);
 
-console.log('Server started on port ' + PORT);
+    app.use(
+        express.json({
+            inflate: true
+        }),
+        express.urlencoded({
+            extended: true,
+            inflate: true
+        }),
+        eventEmitterRouter,
+        streamsRouter
+    );
 
-app.use(
-    express.json({
-        inflate: true
-    }),
-    express.urlencoded({
-        extended: true,
-        inflate: true
-    }),
-    eventEmitterRouter,
-    streamsRouter
-);
-
-app.get('/', function (req, res) {
-    res.send("Hello world");
-});
+    app.get('/', function (req, res) {
+        res.send("Hello world");
+    });
 
 
 
-app.listen(PORT);
+    server.listen(PORT);
+}
+catch (e) {
+    console.error(e);
+}
